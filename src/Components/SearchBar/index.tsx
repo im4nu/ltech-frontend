7c1d2d12c739b9f26db4cc6eab311c2b0f89b931
lorran10 <100364059+lorran10@users.noreply.github.com>
2022-11-18 12:38:35 -0300

    alter

---
+++ src/Components/SearchBar/index.tsx
@@ -0,0 +1,9 @@
+import {SearchBar} from './style'
+
+export default function SearchBarComponent(){
+    return(
+        <SearchBar>
+            <div>Barra de busca</div>
+        </SearchBar>
+    )
+}
---
+++ src/Pages/Employees/index.tsx
@@ -0,0 +1,12 @@
+import {TudoQueTiverDentroDaPagina} from './style'
+import SearchBarComponent from '../../Components/SearchBar'
+
+export default function Employess(){
+    return(
+        <TudoQueTiverDentroDaPagina>
+            <SearchBarComponent />
+            <h1>Oi</h1>
+            <p>Você está na página de funcionários</p>
+        </TudoQueTiverDentroDaPagina>
+    )
+}
