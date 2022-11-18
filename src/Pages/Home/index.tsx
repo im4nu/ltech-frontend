1370ea57fd0ead208391cc60810b47a1ead48ff1
lorran10 <100364059+lorran10@users.noreply.github.com>
2022-11-18 13:14:21 -0300

    alter

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -3,15 +3,10 @@ import HeaderNavigation from "../../Components/HeaderNavigartion"
 export default function Dashboard(){
     return(
         <div>
-<<<<<<< HEAD
-            <h1>Wellcome to home dashboard</h1>
-            </div>
-=======
             <HeaderNavigation title="Home" />
             <div>
                 <h1>Wellcome to home dashboard</h1>
             </div>
         </div>
->>>>>>> 376cba0de1dc8376319401d057ee6d6118bc6c42
     )
 }
