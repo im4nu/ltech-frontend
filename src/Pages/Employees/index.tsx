ba2f27f3b9d3d8748feb3ae4d4fc4f4a5e3e5b4f
lorran10 <100364059+lorran10@users.noreply.github.com>
2022-11-19 14:45:03 -0300

    add

--- src/Pages/Employees/index.tsx
+++ src/Pages/Employees/index.tsx
@@ -4,15 +4,6 @@ import Sidebar from '../../Components/Sidebar'
 
 export default function Employess(){
     return(
-<<<<<<< HEAD
-    <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
-        <Sidebar />
-        
-        <div style={{display: "flex", flexDirection: "column"}}>
-            <TudoQueTiverDentroDaPagina>
-                <HeaderNavigation title='Fuincionários'/>
-            </TudoQueTiverDentroDaPagina>
-=======
         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
             <Sidebar />
             
@@ -22,7 +13,6 @@ export default function Employess(){
                     <h1 style={{fontWeight: 500}}>Wellcome to Employess's Screen</h1>
                 </Container>
             </div>
->>>>>>> e86d0b01af4809b912c5e3ab6cace4cfabde7d70
         </div>
     )
 }
