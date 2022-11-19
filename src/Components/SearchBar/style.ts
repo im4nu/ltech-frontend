5cb678ede25bfa0f8836f5244993ced4436d3f52
lorran10 <100364059+lorran10@users.noreply.github.com>
2022-11-19 11:57:57 -0300

    coomit

--- src/Components/SearchBar/style.ts
+++ src/Components/SearchBar/style.ts
@@ -8,21 +8,19 @@ export const SearchBar = styled.div`
 
 export const Input = styled.input`
 background: #F0F2F5;
+position:absolute;
 outline:none;
 border-radius: 7px;
 height: 1.688rem;
-width: 12vw;
+width: 15vw;
 border: 0 none;
 margin-left:2.5rem;
 display:flex;
-justify-content:center;
-margin-top:-1.8rem;
-color:#747874;
-
-
+top:0.1rem
 `;
 export const Divinput = styled.div`
 margin-left: 34.5rem;
+position:relative;
 margin-top: 1rem;
 background-color:#F0F2F5;
 border-radius: 7px;
@@ -30,7 +28,6 @@ width: 19vw;
 height:1.875rem;
 box-shadow: 3px 4px 4px rgba(0, 0, 0, 0.25);
 `;
-
 
 export const Titlefun = styled.h2`
 diplay:block;
@@ -42,7 +39,7 @@ margin-top:3rem;
 `;
 export const ButtonSearch = styled.img`
 margin-left:0.5rem;
-margin-top:0.5rem;
+margin-top:0.4rem;
 cursor:pointer;
 
 `;
