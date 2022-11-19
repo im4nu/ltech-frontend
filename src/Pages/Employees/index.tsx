c331a6a75c1b0607a3d6d207f113c1dee2612b02
lorran10 <100364059+lorran10@users.noreply.github.com>
2022-11-19 10:23:27 -0300

    commit

--- src/Pages/Employees/index.tsx
+++ src/Pages/Employees/index.tsx
@@ -1,26 +1,12 @@
 import {TudoQueTiverDentroDaPagina} from './style'
 import SearchBarComponent from '../../Components/SearchBar'
-<<<<<<< HEAD
-import HeaderNavigartion from '../../Components/HeaderNavigartion'
-=======
 import HeaderNavigation from '../../Components/HeaderNavigation'
->>>>>>> 3450be844e7a69651b279ef81558a102fdc491bf
 
 export default function Employess(){
     return(
         <TudoQueTiverDentroDaPagina>
-<<<<<<< HEAD
-            
-         <HeaderNavigartion>
-    
-            <SearchBarComponent />
-            
-            
-            
-=======
             <HeaderNavigation title='Fuincionários'/>
             <SearchBarComponent />            
->>>>>>> 3450be844e7a69651b279ef81558a102fdc491bf
         </TudoQueTiverDentroDaPagina>
     )
 }
