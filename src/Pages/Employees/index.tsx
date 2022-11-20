62f0ea3253d1183d97553f29d76208ab497ffd79
lorran10 <100364059+lorran10@users.noreply.github.com>
2022-11-20 11:56:46 -0300

    table

--- src/Pages/Employees/index.tsx
+++ src/Pages/Employees/index.tsx
@@ -1,7 +1,11 @@
 import {Container} from './style'
 import HeaderNavigation from '../../Components/HeaderNavigation'
 import Sidebar from '../../Components/Sidebar'
-
+import {Table} from './style'
+import {Divheade} from './style'
+import {Td} from './style'
+import {Tbody} from './style'
+import  {DivTable}from './style'      
 export default function Employess(){
     return(
         <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
@@ -13,6 +17,41 @@ export default function Employess(){
                     <h1 style={{fontWeight: 500}}>Wellcome to Employess's Screen</h1>
                     
                 </Container>
+            <DivTable>
+                <Table>
+                <Divheade>
+                    
+                      <th>Nome</th>
+                      <th>Telefone</th>
+                      <th>CPF</th>
+                      <th>Cidade</th>
+
+                </Divheade>
+                <Tbody>
+                    <tr>
+                      <Td>Loran de Sousa Oliveira</Td>
+                      <Td> (88)994699583</Td>
+                      <Td>62406459312</Td>
+                      <Td>Juazeiro do Norte</Td>
+                      
+                    </tr>
+                    <tr>
+                      <Td>Loran de Sousa Oliveira</Td>
+                      <Td> (88)994699583</Td>
+                      <Td>62406459312</Td>
+                      <Td>Juazeiro do Norte</Td>
+                       
+                    </tr>
+                </Tbody>
+                <tfoot>
+                      <Td>Loran de Sousa Oliveira</Td>
+                      <Td> (88)994699583</Td>
+                      <Td>62406459312</Td>
+                      <Td>Juazeiro do Norte</Td>
+                     
+                </tfoot>
+                </Table>
+                </DivTable>
             </div>
         </div>
     )
--- src/Pages/Employees/style.ts
+++ src/Pages/Employees/style.ts
@@ -3,3 +3,30 @@ import styled from "styled-components";
 export const Container = styled.div`
     padding: 10px 5vw;
 `;
+export const Table = styled.table`
+      margin-top:7rem;
+      width: 60vw;
+     
+`;
+export const Divheade = styled.div`
+background-color: #363740;
+height: 7vh;
+color: #F0F2F5;    
+`;
+export const Td = styled.td`
+padding-left:0.4rem;
+background-color:#FFFFFF;
+height: 7vh;
+font-size:14px;
+      
+`;
+export const Tbody = styled.tbody`
+
+
+`;
+
+export const DivTable = styled.div`
+display:flex;
+justify-content:center;
+
+`;
